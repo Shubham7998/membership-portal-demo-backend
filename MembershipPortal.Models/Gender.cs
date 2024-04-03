ae5c94426f80b0c2be59ce5478bc6142ee48ee87
Shubham7998 <shubhampatil7998@gmail.com>
2024-04-03 15:11:12 +0530

    Models, DTO and DbContext Done For Subscriber and Gender

---
+++ MembershipPortal.Models/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MembershipPortal.Models
+{
+    [Table("Genders")]
+    public class Gender
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Gender")]
+        [MaxLength(10)]
+        public string GenderName { get; set; }
+
+    }
+}
---
+++ MembershipPortal.Models/Subscriber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.Models
+{
+    [Table("Subscribers")]
+    public class Subscriber
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter first name")]
+        [MaxLength(50)]                      
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Customer number")]
+        public string ContactNumber { get; set; }
+
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Please enter Customer Email")]
+        public string Email { get; set; }
+
+        [ForeignKey("Gender")]
+        [Required(ErrorMessage ="Please select valid gender")]
+        public long GenderId { get; set;}
+
+        public virtual Gender Gender { get; set; }
+    }
+}
