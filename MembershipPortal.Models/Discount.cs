120d9b7c86669bf1d3cab563cceed43f2d1ee4a7
Hemant-Dev <phemant092@gmail.com>
2024-04-03 15:10:38 +0530

    Added Tax , Discount, Discount Mode Model and DTO

---
+++ MembershipPortal.Models/Discount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.Models
+{
+    [Table("Discounts")]
+    public class Discount
+    {
+        [Required(ErrorMessage = "Discount Id is required")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Discount Code is required")]
+        [MaxLength(10, ErrorMessage = "Discount code cannot be greater than length 10")]
+        public string DiscountCode { get; set; }
+        [Required(ErrorMessage = "Discount Amount is required")]
+        
+        public decimal DiscountAmount { get; set; }
+        [Required(ErrorMessage = "DiscountModeId is required")]
+        [ForeignKey(nameof(DiscountMode))]
+        public long DiscountModeId { get; set; }
+        public virtual DiscountMode DiscountMode { get; set; }
+
+    }
+}
