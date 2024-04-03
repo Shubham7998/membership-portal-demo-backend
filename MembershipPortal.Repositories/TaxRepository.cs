8f4b8f9be903c0aae0ebb722449d95863a0e8f53
Hemant-Dev <phemant092@gmail.com>
2024-04-03 16:43:12 +0530

    Added Tax Irepo and Tax Repo

---
+++ MembershipPortal.Repositories/TaxRepository.cs
@@ -0,0 +1,38 @@
+﻿using MembershipPortal.IRepositories;
+using MembershipPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipPortal.Repositories
+{
+    public class TaxRepository : IRepository<Tax>, ITaxRepository
+    {
+        public Task<Tax> CreateAsync(Tax entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Tax entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tax>> GetAsyncAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tax> GetAsyncById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tax> UpdateAsync(Tax entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
