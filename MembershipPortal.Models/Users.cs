9ba091ea40f0399b14f38cff0e81e473d23d844e
Hemant-Dev <phemant092@gmail.com>
2024-04-03 14:19:13 +0530

    test Sync 2

--- MembershipPortal.Models/Users.cs
+++ MembershipPortal.Models/Users.cs
@@ -1,7 +1,9 @@
-﻿namespace MembershipPortal.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MembershipPortal.Models
 {
     public class Users
     {
-
+        public int MyProperty { get; set; }
     }
 }
