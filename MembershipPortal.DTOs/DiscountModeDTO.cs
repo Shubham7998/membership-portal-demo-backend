53a489ab712e50daa699720c3137ebb4f4e3d2b0
Shubham7998 <shubhampatil7998@gmail.com>
2024-04-03 15:35:42 +0530

    DTO changes added maxlength changes

--- MembershipPortal.DTOs/DiscountModeDTO.cs
+++ MembershipPortal.DTOs/DiscountModeDTO.cs
@@ -8,11 +8,13 @@ using System.Threading.Tasks;
 namespace MembershipPortal.DTOs
 {
     public record CreateDiscountModeDTO(
-            [Required(ErrorMessage = "Discount Mode Type is Required")] string DiscountModeType
+            [Required(ErrorMessage = "Discount Mode Type is Required")]
+            [MaxLength(100, ErrorMessage = "Max Length of Discount Type is 100 only")] string DiscountModeType
         );
     public record UpdateDiscountModeDTO(
             [Required(ErrorMessage = "Discount Mode Id is required")] long Id,
-            [Required(ErrorMessage = "Discount Mode Type is Required")] string DiscountModeType
+            [Required(ErrorMessage = "Discount Mode Type is Required")]
+            [MaxLength(100, ErrorMessage = "Max Length of Discount Type is 100 only")] string DiscountModeType
         );
     public record GetDiscountModeDTO(
             long Id,
